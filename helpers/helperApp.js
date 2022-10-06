00b666bb550984543c9a302cb37d785b179e92ff
Jeferson <jvelasquezc15@miumg.edu.gt>
2022-10-06 16:46:52 -0600

    Lógica del archivo Helper completada

--- helpers/helperApp.js
+++ helpers/helperApp.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
-const fill = require('fill-range');
 /* Archivo para modular la lógica*/
-const crearPrestamo = async(nombre='someone', apellido='example', dpi=1234567890123, nit='12345678',monto=100000,intAnual=0.5, periodo=5, tabla=[]) => {
+const crearPrestamo = async(nombre='someone', apellido='example', dpi=1234567890123, nit='12345678',monto=100000,intAnual=0.0525, periodo=5, tabla=[]) => {
 // **** Entradas *****
 // Capital (monto del préstamo)
 // Interés anual %
@@ -10,74 +9,61 @@ const crearPrestamo = async(nombre='someone', apellido='example', dpi=1234567890
   let mesesArray = []
   let saldoArray = []
   let amortizacionArray = []
-  let auxArray = []
   let interesesArray = []
   let totalArray = []
   saldoArray.push(monto)
 
   try {
-    // **** Proceso ****
+    // Meses sirve para saber la cantidad de veces a iterar
     let meses = periodo * 12
-    let resultadoAmortizacion
-    // meses
-    for (let i = 0; i<=meses; i++) {
+    // **** Meses ****
+    for (let i = 1; i<=meses; i++) {
       mesesArray.push(i)
     }
     
-    // Amortización de capital
+    // **** Amortización de capital ****
     // Amortización = monto / cantidad de meses (es una constante)
-    for (let i = 0; i<=meses; i++) {
+    let resultadoAmortizacion
+    for (let i = 1; i<=meses; i++) {
       resultadoAmortizacion = monto/meses
       amortizacionArray.push(resultadoAmortizacion)
     }
-
-    // Aux
-    /* for (let i = 0; i<=meses; i++) {
-      //console.log(amortizacionArray[i])
-      let nuevoSaldo = 0
-      nuevoSaldo += amortizacionArray[i]
-      if(i > 0) {
-
-      }
-      auxArray.push(nuevoSaldo)
-      
-    } */
+    // **** Saldo ****
+    // Saldo = saldo [i] - amortización de capital
     for (let i = 0; i<=meses; i++) {
       let valor = monto
+      // Desde el monto inicial hasta 0
       while(valor >= resultadoAmortizacion ) {
         const nuevo = valor -= resultadoAmortizacion
-        auxArray.push(nuevo)
+        saldoArray.push(nuevo)
       }
     }
     
-    let resultadoSaldo = auxArray.reduce((a,e) => {
+    saldoArray.push(0)
+    // deja solo los valores que no se repiten
+    let resultadoSaldo = saldoArray.reduce((a,e) => {
       if(!a.find(d => d == e)) {
         a.push(e)
       }
       return a;
     }, [])
 
-
-    console.log(resultadoSaldo)
-    //Saldo
-    // Saldo = saldo [i] - amortización de capital
-/*     for (let i=0; i<=meses; i++) {
-      let saldoRestante = monto-resultadoAmortizacion
-      let nuevoSaldo
-      saldoArray.push(nuevoSaldo)
-    } */
-
     // intereses pagados
-    // Intereses pagados varía = Saldo pendiente / porcentaje mensual (hay que iterar)
-    const algo = fill('0', monto, resultadoAmortizacion)
-    console.log(algo)
+    // Intereses pagados varía = Saldo pendiente * porcentaje mensual (hay que iterar)
+    for (let i = 0; i<=meses; i++) {
+      let interes = 0
+      interes = (resultadoSaldo[i] * intAnual) / 12
+      interesesArray.push(interes)
+    }
     //Total
     // Total = amortización + intereses pagados
-    /* for (let i=0; i<=meses; i++) {
+    for (let i=1; i<=meses; i++) {
+      let total = 0
+      total = interesesArray[i-1] + resultadoAmortizacion
+      totalArray.push(total)
+    }
 
-    } */
-
-    tabla = [{mesesArray},{amortizacionArray},{resultadoSaldo}]
+    tabla = [{mesesArray},{amortizacionArray},{resultadoSaldo},{interesesArray},{totalArray}]
 
   return tabla
   }catch(err) {
