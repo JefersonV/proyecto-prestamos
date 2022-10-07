e044e17679174c5161acd1a8bacccf3897bf80cc
Jeferson <jvelasquezc15@miumg.edu.gt>
2022-10-06 21:46:28 -0600

    Falta adaptar la tabla a la data del archivo HelperApp.js

--- helpers/helperApp.js
+++ helpers/helperApp.js
@@ -1,11 +1,6 @@
 const fs = require('fs')
 /* Archivo para modular la lógica*/
-const crearPrestamo = async(nombre='someone', apellido='example', dpi=1234567890123, nit='12345678',monto=100000,intAnual=0.0525, periodo=5, tabla=[]) => {
-// **** Entradas *****
-// Capital (monto del préstamo)
-// Interés anual %
-// Período en años (plazo)
-// Datos del cliente (nombre, apellido, dpi, nit)
+const crearPrestamo = async(nombre='someone', apellido='example', dpi=1234567890123, nit='12345678',telefono='7762-0000', direccion='Panajachel zona 4 av. Santander', monto=100000,intAnual=0.0525, periodo=5, tabla=[], listar = true) => {
   let mesesArray = []
   let saldoArray = []
   let amortizacionArray = []
@@ -25,7 +20,7 @@ const crearPrestamo = async(nombre='someone', apellido='example', dpi=1234567890
     // Amortización = monto / cantidad de meses (es una constante)
     let resultadoAmortizacion
     for (let i = 1; i<=meses; i++) {
-      resultadoAmortizacion = monto/meses
+      resultadoAmortizacion = (monto/meses).toFixed(2)
       amortizacionArray.push(resultadoAmortizacion)
     }
     // **** Saldo ****
@@ -34,7 +29,7 @@ const crearPrestamo = async(nombre='someone', apellido='example', dpi=1234567890
       let valor = monto
       // Desde el monto inicial hasta 0
       while(valor >= resultadoAmortizacion ) {
-        const nuevo = valor -= resultadoAmortizacion
+        const nuevo = (valor -= resultadoAmortizacion).toFixed(2)
         saldoArray.push(nuevo)
       }
     }
@@ -52,7 +47,7 @@ const crearPrestamo = async(nombre='someone', apellido='example', dpi=1234567890
     // Intereses pagados varía = Saldo pendiente * porcentaje mensual (hay que iterar)
     for (let i = 0; i<=meses; i++) {
       let interes = 0
-      interes = (resultadoSaldo[i] * intAnual) / 12
+      interes = ((resultadoSaldo[i] * intAnual) / 12).toFixed(2)
       interesesArray.push(interes)
     }
     //Total
@@ -62,9 +57,34 @@ const crearPrestamo = async(nombre='someone', apellido='example', dpi=1234567890
       total = interesesArray[i-1] + resultadoAmortizacion
       totalArray.push(total)
     }
+    if(listar) {
+      console.log('-------------------------------')
+      console.log('|Préstamo sobre saldos')
+      console.log('-------------------------------')
+      console.log(`|Nombre: ${nombre} ${apellido}`)
+      console.log(`|DPI: ${dpi} NIT: ${nit}`)
+      console.log(`|Dirección: ${direccion}`)
+      console.log(`|Teléfono: ${telefono}`)
+      console.log('-------------------------------')
+      console.log(`|Monto del préstamo: ${monto}`)
+      console.log(`|Porcentaje de Interés: ${intAnual}`)
+      console.log(`|Período: ${periodo} años`)
+      console.log(`|Período en meses: ${meses}`)
+      console.log('-------------------------------')
+      console.log('|Mes|Intereses pagados |Capital amortizado | Total | Saldo |')
+      for(let i = 0; i<=meses; i++) {
+        console.log(`|${mesesArray[i]}   |${interesesArray[i]}          |${amortizacionArray[i]}     | ${totalArray[i]}    |${resultadoSaldo[i]    }|`)
+      }
+    }
+    // tabla = [{mesesArray},{amortizacionArray},{resultadoSaldo},{interesesArray},{totalArray}]
+    /* let tabla2 = []
+    for(let i = 0; i<=meses; i++) {
+      tabla2 = {
+        mes: mesesArray[i]
+      }
+    } */
 
-    tabla = [{mesesArray},{amortizacionArray},{resultadoSaldo},{interesesArray},{totalArray}]
-
+    // console.log(tabla2)
   return tabla
   }catch(err) {
     throw err
